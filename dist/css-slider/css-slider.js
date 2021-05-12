fe7544a209c914e38188619d53e10df2b613152d
Alex Dimitrov <xavortm@gmail.com>
2021-05-12 10:22:59 +0300

    Add general slider structure

--- dist/css-slider/css-slider.js
+++ dist/css-slider/css-slider.js
@@ -1,5 +1,38 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("ready!");
+  // Collect all HTML nodes we need to make the slider work:
+  var sliders = document.querySelectorAll(".js-slider"); // And simple enough, run the slider function for each one.
+
+  sliders.forEach(function (slider) {
+    simpleSlider(slider);
+  });
 }, false);
+
+function simpleSlider(slider) {
+  var slides = slider.querySelectorAll(".slider-item");
+  var navPrev = slider.querySelector(".js-slide-prev");
+  var navNext = slider.querySelector(".js-slide-next");
+  var currentSlidePosition = 0;
+
+  var sliderInitActive = function sliderInitActive() {
+    var hasCurrentSet = false;
+    slides.forEach(function (slide, index) {
+      if (slide.classList.contains("is-current")) {
+        hasCurrentSet = slide.classList.contains("is-current");
+        currentSlidePosition = index;
+      }
+    });
+    !hasCurrentSet && slides[0].classList.add("is-current");
+  };
+
+  var handleNavigation = function handleNavigation() {
+    if (!navPrev && !navNext) {
+      return;
+    }
+  }; // Let's get the party going
+
+
+  sliderInitActive();
+  handleNavigation();
+}
--- src/css-slider/script.js
+++ src/css-slider/script.js
@@ -1,7 +1,44 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("ready!");
+    // Collect all HTML nodes we need to make the slider work:
+    const sliders = document.querySelectorAll(".js-slider");
+
+    // And simple enough, run the slider function for each one.
+    sliders.forEach((slider) => {
+      simpleSlider(slider);
+    });
   },
   false
 );
+
+function simpleSlider(slider) {
+  const slides = slider.querySelectorAll(".slider-item");
+  const navPrev = slider.querySelector(".js-slide-prev");
+  const navNext = slider.querySelector(".js-slide-next");
+
+  let currentSlidePosition = 0;
+
+  const sliderInitActive = () => {
+    let hasCurrentSet = false;
+
+    slides.forEach((slide, index) => {
+      if (slide.classList.contains("is-current")) {
+        hasCurrentSet = slide.classList.contains("is-current");
+        currentSlidePosition = index;
+      }
+    });
+
+    !hasCurrentSet && slides[0].classList.add("is-current");
+  };
+
+  const handleNavigation = () => {
+    if (!navPrev && !navNext) {
+      return;
+    }
+  };
+
+  // Let's get the party going
+  sliderInitActive();
+  handleNavigation();
+}
